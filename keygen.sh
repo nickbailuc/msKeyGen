049566fd9a64e4f411f12cde44e67edb11d5fab6
Nick <nick@GNU>
2020-07-16 16:56:41 -0400

    v0.40

--- keygen.sh
+++ keygen.sh
@@ -1,30 +1,95 @@
 #!/bin/bash
+#SPDX-License-Identifier: GPL-3.0-or-later
+VERSION=0.40
 
-##  Author = "Nick Bailuc"
-##  Copyright = "Copyright (C) 2020 Nick Bailuc, <nick.bailuc@gmail.com>"
-##  License = "GNU General Public License, version 3 or later"
-##	"Windows 95 Product Key Generator" (Version 0.32)
-##
-##  There are 41713826857560 valid Product Key's for Windows 95. This program
-##  uses Bash's $RANDOM variable in a sequence until it finds appropriate keys.
-##  Inspiration: Youtube <<FlyTech Videos>> https://www.youtube.com/watch?v=3DCEeASKNDk
-##
-##  This program is free software: you can redistribute it and/or modify
-##  it under the terms of the GNU General Public License as published by
-##  the Free Software Foundation, either version 3 of the License, or
-##  (at your option) any later version.
-##
-##  This program is distributed in the hope that it will be useful,
-##  but WITHOUT ANY WARRANTY; without even the implied warranty of
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##  GNU General Public License for more details.
-##
-##  You should have received a copy of the GNU General Public License
-##  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-##
-VERSION=0.32
+help()
+{
+printf "\
+$0 (Version 0.40)
+Copyright (C) 2020 Nick Bailuc, <nick.bailuc@gmail.com>
+This is free software; see the source code for copying conditions.
+There is ABSOLUTELY NO WARRANTY; not even for MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.
 
-generate()
+This program uses the \$RANDOM variable built into Bash in a sequence until it finds
+appropriate license keys for various deprecated Microsoft products from the 1990s.
+
+
+	[SYNOPSIS]
+	$0 ARG1 ARG2 ARG3
+	
+	Example: $0 95o 10
+		Generate 10 keys silently (stdout only keys without enumeration)
+
+	Example: $0 95c 2**5 --silent
+		Calculate 2**5 (=32) and generate that many keys.
+
+
+	[ARG1 PARAMETERS]
+	w95oem
+		Generate Windows 95 Product ID (OEM) Keys.
+
+	w95cd
+		Generate Windows 95 CD Keys.
+
+	wntoem
+		Generate Windows NT 4.0 Workstation Product ID (OEM) Keys.
+
+	wntcd
+		Generate Windows NT 4.0 Server CD Keys.
+
+	-h, --help
+		Show this help and exit 0.
+
+	-l, --license
+		Display license
+
+
+	[ARG2 PARAMETERS]
+	This parameter may be ommited entirely, in which case only 1 key will be generated
+
+	N∈(1, 2**63-1)
+		Enter any integer between 1 and 9223372036854775807 representing
+		how many keys to be generated. A Bash integer calculation may also be entered!
+
+
+	[EXIT CODES]
+	0 : Success
+	1 : Invalid argument: number of keys
+	2 : End of script!
+		The script is set to exit 0 after completion of generation. If the script
+		reaches the end (outside of main() function) the script will exit 2
+		indicating an error has occured!
+
+
+Inspiration:
+https://medium.com/@dgurney/so-you-want-to-generate-license-keys-for-old-microsoft-products-a355c8bf5408
+
+Disclaimer: This script was written for educational purposes only, and the author does not
+encourage or endorse software piracy of any sort.
+"
+}
+
+license()
+{
+printf "\
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"
+}
+
+
+w95oem()
 {
 	# 3-digit day of year:
 	D=$RANDOM
@@ -52,7 +117,7 @@ generate()
 		Y=0$Y
 	fi
 
-	# 7-digit "divisible by 7" group:
+	# 7-digit "divisible by 7" segment:
 	a=0; b=0; c=0; d=0; e=0; f=1
 	while (( (100000*a + 10000*b + 1000*c + 100*d + 10*e + f) % 7 != 0 ))
 	do
@@ -64,7 +129,7 @@ generate()
 		f=$(( RANDOM % 10 ))
 	done
 
-	# 5-digit "random" group:
+	# 5-digit "random" segment:
 	r1=$(( RANDOM % 10 ))
 	r2=$(( RANDOM % 10 ))
 	r3=$(( RANDOM % 10 ))
@@ -75,105 +140,74 @@ generate()
 	printf "$D$Y-OEM-0$a$b$c$d$e$f-$r1$r2$r3$r4$r5\n"
 }
 
-generate_debug()
+
+w95cd()
 {
-	# Define text format:
-	c1=$(tput bold)
-	c2=$(tput sgr0)
-	printf "${c1}Initialization complete!\n\n"
-
-	# 3-digit day of year:
-	printf "Determining 3-digit day of year:${c2}\n"
-	D=$RANDOM
-	while (( D > 365 ))
-	do
-		D=$(( RANDOM % 366 ))
-		printf "\tTrying $D\n"
-	done
-	D=$(( D + 1))
-	if (( D < 10))
-	then
-		D=00$D
-	elif (( D < 100 ))
-	then
-		D=0$D
-	fi
-	printf "${c1}\t3-digit day of year determined to be $D!\n\n"
-
-	# 2-digit year:
-	Y=$RANDOM
-	printf "Determining 2-digit year:${c2}\n"
-	while (( Y > 3 && Y < 95)) || (( Y > 99 ))
-	do
-		Y=$(( RANDOM % 100 ))
-		printf "\tTrying $Y\n"
-	done
-	if (( Y < 4 ))
-	then
-		Y=0$Y
-	fi
-	printf "${c1}\t2-digit year determined to be $Y!\n\n"
-
-	# 7-digit "divisible by 7" group:
-	printf "Initiating divisible by 7 group:${c2}\n"
-	a=0; b=0; c=0; d=0; e=0; f=1
-	while (( (100000*a + 10000*b + 1000*c + 100*d + 10*e + f) % 7 != 0 ))
-	do
-		a=$(( RANDOM % 10 ))
-		b=$(( RANDOM % 10 ))
-		c=$(( RANDOM % 10 ))
-		d=$(( RANDOM % 10 ))
-		e=$(( RANDOM % 10 ))
-		f=$(( RANDOM % 10 ))
-		printf "\ta = $a\n\tb = $b\n\tc = $c\n\td = $d\n\te = $e\n\tf = $f\n"
-		printf "\tLinear combination equals $((100000*a + 10000*b + 1000*c + 100*d + 10*e + f))\n"
-	done
-	printf "${c1}\t$((100000*a + 10000*b + 1000*c + 100*d + 10*e + f)) is divisible by 7!\n\n"
-
-	# 5-digit "random" group:
-	printf "Determining 5-digit group:${c2}\n"
-	r1=$(( RANDOM % 10 ))
-	r2=$(( RANDOM % 10 ))
-	r3=$(( RANDOM % 10 ))
-	r4=$(( RANDOM % 10 ))
-	r5=$(( RANDOM % 10 ))
-	printf "\tr1 = $r1\n\tr2 = $r2\n\tr3 = $r3\n\tr4 = $r4\n\tr5 = $r5\n"
-	printf "5-digit group determined to be $r1$r2$r3$r4$r5!\n\n"
-
-	# Export Product Key:
-	printf "${c1}Parsing variables and exporting to stdout:${c2}\n"
-	printf "\033[4mWindows 95 Product Key #$i:\t$D$Y-OEM-0$a$b$c$d$e$f-$r1$r2$r3$r4$r5\033[0m\n\n"
+	echo TODO
 }
+
+
+wntoem()
+{
+	echo TODO
+}
+
+
+wntcd()
+{
+	echo TODO
+}
+
+
+toString()
+{
+	if [[ $ARG1 == "w95oem" ]]
+	then printf "Windows 95 OEM Product ID #$i:\t"
+	elif [[ $ARG1 == "w95cd" ]]
+	then printf "Windows 95 CD Key #$i:\t"
+	elif [[ $ARG1 == "wntoem" ]]
+	then printf "Windows NT 4.0 Product ID #$i:\t"
+	elif [[ $ARG1 == "wntcd" ]]
+	then printf "Windows NT 4.0 CD Key #$i:\t"
+	fi
+}
+
 
 main()
 {
-	if [[ $ARG1 == "-d" ]] || [[ $ARG1 == "--debug" ]]
+	if [[ $ARG1 == "-h" ]] || [[ $ARG1 == "--help" ]]
 	then
-		generate_debug
+		help
 		exit 0
+	elif [[ $ARG1 == "-l" ]] || [[ $ARG1 == "--license" ]]
+	then
+		license
+		exit 0
+
+	# License Key Generation:
 	else
-		if [[ $ARG1 -eq "" ]]
+		if [[ $ARG2 -eq "" ]]
 		then
 			i=1
-			if [[ $ARG2 != "-s" ]] && [[ $ARG2 != "--silent" ]]
+			if [[ $ARG3 != "-s" ]] && [[ $ARG3 != "--silent" ]]
 			then
-				printf "Windows 95 Product Key #$i:\t"
+				toString
 			fi
-			generate
+			$ARG1
 			exit 0
-		elif (( ARG1 < 1 ))
+		elif (( ARG2 < 1 ))
 		then
 			printf "Invalid input! Please enter an integer between 1 and (2^63)-1 (inclusively)\n" >&2
 			exit 1
 		else
 			i=1
-			while (( i <= ARG1 ))
+			while (( i <= ARG2 ))
 			do
-				if [[ $ARG2 != "-s" ]] && [[ $ARG2 != "--silent" ]]
+				if [[ $ARG3 != "-s" ]] && [[ $ARG3 != "--silent" ]]
 				then
-					printf "Windows 95 Product Key #$i:\t"
+					toString
 				fi
-				generate
+				$ARG1
 				i=$(( i + 1 ))
 			done
 			exit 0
@@ -184,13 +218,9 @@ main()
 # Initialization:
 ARG1=$1
 ARG2=$2
+ARG3=$3
 main
 printf "End of script error!\n" >&2
 exit 2
-
-# Exit Codes:
-#	0 : Success
-#	1 : Invalid argument: number of keys
-#	2 : End of script!
 
 #TODO: add validator
